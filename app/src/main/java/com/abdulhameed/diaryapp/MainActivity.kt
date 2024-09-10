6c5c3079798a4ccae7751b630d24eca3014f1bdd
FAWRY\AbdulHameed.Mohamed <abdulhameed.mohamed@fawry.com>
2024-09-10 15:05:53 +0300

    Initial commit

---
+++ app/src/main/java/com/abdulhameed/diaryapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.abdulhameed.diaryapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.abdulhameed.diaryapp.ui.theme.DiaryAppTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DiaryAppTheme {
+                Greeting(name = "AbdulHameed")
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hello $name In Modularization",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        Text(
+            text = "Write Your Diary And Feeling About It",
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(top = 40.dp)
+        )
+    }
+}
